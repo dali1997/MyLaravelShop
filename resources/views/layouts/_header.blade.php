9e989b4c66ab254f69fed0d03437f577ff265a91
vagrant <vagrant@homestead>
2019-04-12 07:06:55 +0000

    基础布局

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+    <div class="container">
+        <!-- 品牌形象 -->
+        <a class="navbar-brand" href="{{ url('/') }}">
+            单店铺商城
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- 左侧导航栏 -->
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+
+            <!-- 右侧导航栏 -->
+            <ul class="navbar-nav navbar-right">
+                <!-- 验证链接 -->
+                <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
