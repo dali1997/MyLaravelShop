816a276f681e0a8ec420b1a0adf87e21b4288214
vagrant <vagrant@homestead>
2019-04-15 01:16:03 +0000

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/','PagesController@root')->name('root');
-Auth::routes();
+Route::get('/','PagesController@root')->name('root')->middleware('verified');
+Auth::routes(['verify' => true]);
 
 
