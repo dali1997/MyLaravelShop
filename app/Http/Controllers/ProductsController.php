13fd09a272ee6da3dc98d51f6650ec2ae4cd66a4
vagrant <vagrant@homestead>
2019-04-18 03:35:04 +0000

    用户界面 - 商品筛选、排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,7 +9,43 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $products=Product::query()->where('on_sale',true)->paginate(16);
-        return view('products.index',['products'=>$products]);
+        //创建一个查询构造器
+        $builder = Product::query()->where('on_sale', true);
+        //判断是否有提交search参数，如果有就赋值给$search变量
+        //search参数用来模糊搜索商品
+        if ($search = $request->input('search', '')) {
+            $like = '%' . $search . '%';
+            //迷糊搜索商品标题、商品详情、sku标题、sku描述
+            $builder->where(function ($query) use ($like) {
+                $query->where('title', 'like', $like)
+                    ->orwhere('description', 'like', $like)
+                    ->orwhereHas('skus', function ($query) use ($like) {
+                        $query->where('title', 'like', $like)
+                            ->orwhere('description', 'like', $like);
+                    });
+            });
+        }
+
+        //是否有调教order参数，如果哟就赋值给$order变量
+        //order参数用来控制商品的排序规则
+        if ($order = $request->input('order', '')) {
+            //是否是以_asc或者_desc结尾
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    // 根据传入的排序值来构造排序参数
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        $products = $builder->paginate(16);
+        return view('products.index', [
+            'products' => $products,
+            'filters'=>[
+                "search"=>$search,
+                'order'=>$order,
+            ]
+        ]);
     }
 }
